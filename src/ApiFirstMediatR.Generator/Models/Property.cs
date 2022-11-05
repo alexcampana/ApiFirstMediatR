050dbbe68822bc99d7af82eec827df8a98fedf03
Alex Campana <campana.alex@gmail.com>
2022-11-05 17:55:19 -0400

    Added JsonPropertyName to ensure correct serialization with spec. Also added CancellationToken

--- src/ApiFirstMediatR.Generator/Models/Property.cs
+++ src/ApiFirstMediatR.Generator/Models/Property.cs
@@ -3,6 +3,7 @@ namespace ApiFirstMediatR.Generator.Models;
 internal class Property
 {
     public string? Name { get; set; }
+    public string? JsonName { get; set; }
     public virtual bool IsNullable { get; set; } = true;
     public string? DataType { get; set; }
 }
