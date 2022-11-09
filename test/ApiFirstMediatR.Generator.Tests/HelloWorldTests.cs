f73a0d599b65b799b064e794957598ae787c5667
Alex Campana <campana.alex@gmail.com>
2022-11-09 15:20:21 -0500

    Added tests for 204 responses

---
+++ test/ApiFirstMediatR.Generator.Tests/HelloWorldTests.cs
@@ -0,0 +1,280 @@
+namespace ApiFirstMediatR.Generator.Tests;
+
+public class HelloWorldTests : TestBase
+{
+    [Fact]
+    public void ValidYaml3APISpec_GeneratesValidCode()
+    {
+        var additionalText = new AdditionalTextYml("api_spec.yml", Yaml3ApiSpec) as AdditionalText;
+        ValidateSpec(additionalText);
+    }
+    
+    [Fact]
+    public void ValidJson3APISpec_GeneratesValidCode()
+    {
+        var additionalText = new AdditionalTextYml("api_spec.json", Json3ApiSpec) as AdditionalText;
+        ValidateSpec(additionalText);
+    }
+    
+    [Fact]
+    public void ValidYaml2APISpec_GeneratesValidCode()
+    {
+        var additionalText = new AdditionalTextYml("api_spec.yml", Yaml2ApiSpec) as AdditionalText;
+        ValidateSpec(additionalText);
+    }
+
+    [Fact]
+    public void ValidJson2APISpec_GeneratesValidCode()
+    {
+        var additionalText = new AdditionalTextYml("api_spec.json", Json2ApiSpec) as AdditionalText;
+        ValidateSpec(additionalText);
+    }
+
+    private void ValidateSpec(AdditionalText additionalText)
+    {
+        var code = "namespace HelloWorld;";
+        var inputCompilation = CreateCompilation("HelloWorld", code);
+
+        var generator = new ApiSourceGenerator();
+        var driver = CSharpGeneratorDriver
+            .Create(generator)
+            .AddAdditionalTexts(ImmutableArray.Create(additionalText))
+            .RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation,
+                out var diagnostics);
+        
+        Assert.Empty(diagnostics);
+        
+        var runResult = driver.GetRunResult();
+        Assert.Single(runResult.Results);
+        
+        var generatedSources = runResult.Results.First().GeneratedSources;
+        Assert.Equal(3, generatedSources.Length);
+
+        var dtos = generatedSources.Where(g => g.HintName == "Dtos_HelloWorldDto.g.cs").ToList();
+        Assert.Single(dtos);
+
+        var mediatrRequests = generatedSources.Where(g => g.HintName == "MediatorRequests_GetHelloWorldQuery.g.cs").ToList();
+        Assert.Single(mediatrRequests);
+
+        var controllers = generatedSources.Where(g => g.HintName == "Controllers_ApiController.g.cs").ToList();
+        Assert.Single(controllers);
+
+        var dtoExpectedResult = CSharpSyntaxTree.ParseText(ExpectedDto);
+        Assert.True(dtoExpectedResult.IsEquivalentTo(dtos.First().SyntaxTree));
+
+        var mediatrExpectedResult = CSharpSyntaxTree.ParseText(ExpectedMediatorRequest);
+        Assert.True(mediatrExpectedResult.IsEquivalentTo(mediatrRequests.First().SyntaxTree));
+
+        var controllerExpectedResult = CSharpSyntaxTree.ParseText(ExpectedController);
+        Assert.True(controllerExpectedResult.IsEquivalentTo(controllers.First().SyntaxTree));
+    }
+    
+    private const string Yaml3ApiSpec = @"openapi: 3.0.1
+info:
+  title: HelloWorld API
+  version: v1
+paths:
+  /api/HelloWorld:
+    get:
+      tags:
+        - HelloWorld
+      operationId: GetHelloWorld
+      description: Gets a HelloWorld Message
+      parameters: []
+      responses:
+        200:
+          description: Hello world!
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/HelloWorldDto'
+components:
+  schemas:
+    HelloWorldDto:
+      type: object
+      properties:
+        message:
+          type: string
+          nullable: true
+security: []";
+
+    private const string Json3ApiSpec = @"{
+  ""openapi"": ""3.0.1"",
+    ""info"": {
+        ""title"": ""HelloWorld API"",
+        ""version"": ""v1""
+    },
+""paths"": {
+    ""/api/HelloWorld"": {
+        ""get"": {
+            ""tags"": [
+            ""HelloWorld""
+                ],
+            ""operationId"": ""GetHelloWorld"",
+            ""description"": ""Gets a HelloWorld Message"",
+            ""parameters"": [],
+            ""responses"": {
+                ""200"": {
+                    ""description"": ""Hello world!"",
+                    ""content"": {
+                        ""application/json"": {
+                            ""schema"": {
+                                ""$ref"": ""#/components/schemas/HelloWorldDto""
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+},
+""components"": {
+    ""schemas"": {
+        ""HelloWorldDto"": {
+            ""type"": ""object"",
+            ""properties"": {
+                ""message"": {
+                    ""type"": ""string"",
+                    ""nullable"": true
+                }
+            }
+        }
+    }
+},
+""security"": []
+}";
+
+    private const string Yaml2ApiSpec = @"swagger: 2.0
+info:
+  title: HelloWorld API
+  version: v1
+basePath: /
+produces:
+  - application/json
+paths:
+  /api/HelloWorld:
+    get:
+      description: Gets a HelloWorld Message
+      operationId: GetHelloWorld
+      parameters: []
+      tags:
+        - HelloWorld
+      responses:
+        200:
+          description: Hello world!
+          schema:
+            $ref: '#/definitions/HelloWorldDto'
+definitions:
+  HelloWorldDto:
+    required: []
+    properties:
+      message:
+        type: string
+";
+
+    private const string Json2ApiSpec = @"{
+  ""swagger"": ""2.0"",
+  ""info"": {
+    ""title"": ""HelloWorld API"",
+    ""version"": ""v1""
+  },
+  ""basePath"": ""/"",
+  ""produces"": [
+    ""application/json""
+  ],
+  ""paths"": {
+    ""/api/HelloWorld"": {
+      ""get"": {
+        ""description"": ""Gets a HelloWorld Message"",
+        ""operationId"": ""GetHelloWorld"",
+        ""parameters"": [],
+        ""tags"": [
+          ""HelloWorld""
+        ],
+        ""responses"": {
+          ""200"": {
+            ""description"": ""Hello world!"",
+            ""schema"": {
+              ""$ref"": ""#/definitions/HelloWorldDto""
+            }
+          }
+        }
+      }
+    }
+  },
+  ""definitions"": {
+    ""HelloWorldDto"": {
+      ""required"": [],
+      ""properties"": {
+        ""message"": {
+          ""type"": ""string""
+        }
+      }
+    }
+  }
+}";
+
+    private const string ExpectedDto = @"// <auto-generated/>
+#nullable enable
+#pragma warning disable CS8618
+
+namespace HelloWorld.Dtos
+{
+    public class HelloWorldDto 
+    {
+        [System.Text.Json.Serialization.JsonPropertyName(""message"")]
+        public string? Message { get; set; }
+    }
+}
+
+#pragma warning restore CS8618";
+
+    private const string ExpectedMediatorRequest = @"// <auto-generated/>
+#nullable enable
+using HelloWorld.Dtos;
+
+namespace HelloWorld.Requests
+{
+    /// <summary>
+    /// Gets a HelloWorld Message
+    /// </summary>
+    /// <returns>Hello world!</returns>
+    public sealed class GetHelloWorldQuery : MediatR.IRequest<HelloWorldDto>
+    {
+        public GetHelloWorldQuery()
+        {
+        }
+    }
+}";
+
+    private const string ExpectedController = @"// <auto-generated/>
+#nullable enable
+using HelloWorld.Dtos;
+using HelloWorld.Requests;
+
+namespace HelloWorld.Controllers
+{
+    public sealed class ApiController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly MediatR.IMediator _mediator;
+
+        public ApiController(MediatR.IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        /// <summary>
+        /// Gets a HelloWorld Message
+        /// </summary>
+        /// <returns>Hello world!</returns>
+        [Microsoft.AspNetCore.Mvc.HttpGet(""/api/HelloWorld"")]
+        [Microsoft.AspNetCore.Mvc.ProducesResponseType(typeof(HelloWorldDto), Microsoft.AspNetCore.Http.StatusCodes.Status200OK)]
+        public async System.Threading.Tasks.Task<Microsoft.AspNetCore.Mvc.ActionResult<HelloWorldDto>> GetHelloWorld(System.Threading.CancellationToken cancellationToken)
+        {
+            var request = new GetHelloWorldQuery();
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+    }
+}";
+}
